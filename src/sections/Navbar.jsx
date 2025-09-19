f947b22935c32d7f9d1dd6309c3b31f2de595d3d
Sandesh <sandybatmam@gmail.com>
2025-09-19 18:25:35 +0530

    Hero section motion update

--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -23,7 +23,8 @@ function Navigation(){
 }
 const Navbar = () => {
   const[isOpen, setIsOpen]=useState(false)
-  return <div className='fixed inset-x-0 z-20 w-full backdrop-blur-lg
+  return <div 
+  className='fixed inset-x-0 z-20 w-full backdrop-blur-lg
   bg-primary/40'>
     <div className='mx-auto c-space max-w-7xl'>
       <div className='flex items-center justify-between py-2
